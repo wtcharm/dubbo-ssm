b9e4d5102d948c7adfe12b88727294325bf18e59
handsomewangtao <handsomethewt@163.com>
2017-10-30 23:50:05 +0800

    实现登录验证

--- mldndubbo-consumer/src/test/java/cn/mldn/mldndubbo/consumer/AppTest.java
+++ mldndubbo-consumer/src/test/java/cn/mldn/mldndubbo/consumer/AppTest.java
@@ -4,14 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
     extends TestCase
 {
     /**
-     * Create the test case
+     * Create      the test case
      *
      * @param testName name of the test case
      */
